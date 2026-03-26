c0b3a7e9ea27c6fbbef45392ac6aa17f7fe34b3e
Aymeri Tourneur <27858195+Yoshiip@users.noreply.github.com>
2026-03-26 10:57:46 +0100

    change background search image

--- bookaway-front/src/pages/HomePage.tsx
+++ bookaway-front/src/pages/HomePage.tsx
@@ -4,11 +4,14 @@ import { DayPicker, type DateRange } from "react-day-picker";
 import "react-day-picker/style.css";
 import { useState } from "react";
 import { fr } from "react-day-picker/locale";
+import { format } from "date-fns";
+import { fr as fnsFR } from "date-fns/locale";
 
 
 export function HomePage() {
     const { t } = useTranslation();
-    return <main className="flex flex-col items-center justify-center bg-hero">
+    return <main className="flex flex-col relative text-white items-center -z-2 h-full bg-center bg-cover bg-no-repeat justify-center bg" style={{ backgroundImage: "url('background.jpg')" }}>
+        <div className="bg-black/50 absolute left-0 top-0 right-0 bottom-0 -z-1"></div>
         <h2 className="text-3xl font-medium">Vos prochaines vacances commencent aujourd'hui</h2>
         <h3>avec BookAway</h3>
         <SearchBar />
@@ -19,7 +22,7 @@ export function HomePage() {
 
 function SearchBar() {
     return (
-        <form className="flex gap-4 items-center justify-center border shadow rounded-xl py-2 px-8 bg-gray-200 border-gray-400">
+        <form className="flex gap-4 items-center justify-center border shadow rounded-xl py-2 px-8 bg-gray-200 border-gray-400 text-gray-900">
             <FormDatePart />
             <div>|</div>
             <div className="flex flex-col bg-gray-50 px-4 py-2 rounded-xl">
@@ -41,7 +44,9 @@ function FormDatePart() {
                 setOpenCalendar(!openCalendar);
             }} className="flex flex-col bg-gray-50 px-4 py-2 rounded-xl">
                 <span className="font-bold text-sm">Date du voyage</span>
-                <span>Selectionner une date...</span>
+                {selected?.from && selected.to ?
+                    <span>du {format(selected.from, "d MMM", { locale: fnsFR })} au {format(selected.to, "d MMM", { locale: fnsFR })}</span> :
+                    <span>Selectionner une date...</span>}
             </button>
             {openCalendar &&
                 <div className="absolute bg-white shadow-xl rounded-xl top-16">
@@ -55,13 +60,9 @@ function FormDatePart() {
                         showOutsideDays
                         showWeekNumber
                         timeZone="Europe/Paris"
-                        weekStartsOn={1}
                         selected={selected}
                         onSelect={setSelected}
                         locale={fr}
-                        footer={
-                            selected ? `Selected: ${selected.from?.toLocaleDateString()}` : "Pick a day."
-                        }
                     />
                 </div>
             }
