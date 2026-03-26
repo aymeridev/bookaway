44fc102ef88d91cafc453aacdda3105483d92b97
Aymeri Tourneur <27858195+Yoshiip@users.noreply.github.com>
2026-03-26 11:46:08 +0100

    create property controller

---
+++ bookaway/database/migrations/2026_03_26_103643_create_properties_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 100);
+            $table->enum("type", ['camping', 'hotel']);
+            $table->integer("capacity");
+            $table->json('images');
+            $table->string("description");
+            $table->integer("base_price");
+            $table->integer("price_per_night");
+            $table->json('amenities');
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
