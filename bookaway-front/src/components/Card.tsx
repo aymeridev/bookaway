e03fcf6b494b6279a309c77637fa84ae81cd1a62
Aymeri Tourneur <27858195+Yoshiip@users.noreply.github.com>
2026-04-08 14:04:42 +0200

    login page add background

--- bookaway-front/src/components/Card.tsx
+++ bookaway-front/src/components/Card.tsx
@@ -1,7 +1,8 @@
-import type { ReactNode } from "react";
+import type { HTMLAttributes } from "react";
+import { twMerge } from 'tailwind-merge';
 
-export function Card({ children }: { children: ReactNode }) {
-    return <div className="bg-white rounded-lg shadow-md p-4">
+export function Card({ children, className, ...props }: HTMLAttributes<HTMLDivElement>) {
+    return <div {...props} className={twMerge("bg-white rounded-lg shadow-md p-4", className)}>
         {children}
     </div>
 }
--- bookaway-front/src/pages/LoginPage.tsx
+++ bookaway-front/src/pages/LoginPage.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import api from "../api/axios"; // Ton instance axios créée précédemment
 import { Link, useNavigate } from "react-router";
 import { useAuth } from "../context/AuthContext";
+import { Card } from "../components/Card";
 
 export function LoginPage() {
     const [email, setEmail] = useState("");
@@ -16,12 +17,12 @@ export function LoginPage() {
 
         try {
             const response = await api.post("/login", { email, password });
-            
+
             const token = response.data.token;
             const userData = response.data.user;
 
-            login(token, userData); 
-            
+            login(token, userData);
+
             navigate("/");
         } catch (err: any) {
             setError("Identifiants incorrects. Veuillez réessayer.");
@@ -29,49 +30,53 @@ export function LoginPage() {
     };
 
     return (
-        <main className="flex items-center justify-center h-[80vh]">
-            <form onSubmit={handleLogin} className="bg-white p-8 rounded-2xl shadow-lg border w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-6 text-gray-800 text-center">Connexion à BookAway</h2>
-                
-                {error && <p className="text-red-500 text-sm mb-4 bg-red-50 p-2 rounded">{error}</p>}
+        <main className="relative flex items-center justify-center h-full bg-cover bg-center bg-no-repeat bg-hero">
+            <div className="absolute inset-0 bg-black/65 pointer-events-none"></div>
 
-                <div className="flex flex-col gap-4">
-                    <div className="flex flex-col gap-1">
-                        <label className="text-sm font-semibold text-gray-600">Email</label>
-                        <input 
-                            type="email" 
-                            className="border p-2 rounded-lg outline-blue-500 text-gray-900"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                    </div>
+            <Card className="z-10">
+                <form onSubmit={handleLogin}>
+                    <h2 className="text-2xl font-bold mb-6 text-gray-800 text-center">Connexion à BookAway</h2>
 
-                    <div className="flex flex-col gap-1">
-                        <label className="text-sm font-semibold text-gray-600">Mot de passe</label>
-                        <input 
-                            type="password" 
-                            className="border p-2 rounded-lg outline-blue-500 text-gray-900"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </div>
+                    {error && <p className="text-red-500 text-sm mb-4 bg-red-50 p-2 rounded">{error}</p>}
 
-                    <button type="submit" className="bg-blue-600 text-white py-2 rounded-lg font-bold hover:bg-blue-700 transition mt-2">
-                        Se connecter
-                    </button>
-                    <div className="mt-6 text-center text-sm text-gray-600">
-                        Pas encore de compte ?{" "}
-                        <Link
-                            to="/register" 
-                            className="text-blue-600 font-bold hover:underline"
-                        >
-                            Inscrivez-vous gratuitement
-                        </Link>
+                    <div className="flex flex-col gap-4">
+                        <div className="flex flex-col gap-1">
+                            <label className="text-sm font-semibold text-gray-600">Email</label>
+                            <input
+                                type="email"
+                                className="border p-2 rounded-lg outline-blue-500 text-gray-900"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <div className="flex flex-col gap-1">
+                            <label className="text-sm font-semibold text-gray-600">Mot de passe</label>
+                            <input
+                                type="password"
+                                className="border p-2 rounded-lg outline-blue-500 text-gray-900"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <button type="submit" className="bg-blue-600 text-white py-2 rounded-lg font-bold hover:bg-blue-700 transition mt-2">
+                            Se connecter
+                        </button>
+                        <div className="mt-6 text-center text-sm text-gray-600">
+                            Pas encore de compte ?{" "}
+                            <Link
+                                to="/register"
+                                className="text-blue-600 font-bold hover:underline"
+                            >
+                                Inscrivez-vous gratuitement
+                            </Link>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </Card>
         </main>
     );
 }
