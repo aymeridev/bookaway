a535dbb1a077c3af51b8e5b7dff66e46418098d1
Aymeri Tourneur <27858195+Yoshiip@users.noreply.github.com>
2026-05-20 10:53:05 +0200

    add react-dropzone

--- bookaway-front/src/components/create_property/steps/ImagesStep.tsx
+++ bookaway-front/src/components/create_property/steps/ImagesStep.tsx
@@ -1,37 +1,81 @@
 import { useFieldArray, type UseFormReturn } from "react-hook-form";
-import { Card } from "../../Card";
 import type { PropertyForm } from "../form";
-import { X } from "lucide-react";
+import { useDropzone } from 'react-dropzone';
 
 export function ImagesStep({ form }: { form: UseFormReturn<PropertyForm, any, PropertyForm> }) {
-    const { register, control } = form;
-    const { fields, append, remove } = useFieldArray({
-        control,
-        name: "images"
+    // const { register, control } = form;
+    // const { fields, append, remove } = useFieldArray({
+    //     control,
+    //     name: "images"
+    // });
+
+    const {
+        acceptedFiles,
+        fileRejections,
+        getRootProps,
+        getInputProps
+    } = useDropzone({
+        accept: {
+            'image/jpeg': [],
+            'image/png': []
+        }
     });
 
-    return <Card>
-        <label className="block">
-            <span className="text-gray-700">Images</span>
+    const acceptedFileItems = acceptedFiles.map(file => (
+        <li key={file.path}>
+            {file.path} - {file.size} bytes
+        </li>
+    ));
+
+    const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+        <li key={file.path}>
+            {file.path} - {file.size} bytes
             <ul>
-                {fields.map((field, index) => (
-                    <li key={field.id}>
-                        <input type="url" placeholder="https://..."
-                            {...register(`images.${index}.url` as const)}
-                        />
-                        <button type="button" onClick={() => remove(index)}>
-                            <X className="size-5" />
-                        </button>
-                    </li>
+                {errors.map(e => (
+                    <li key={e.code}>{e.message}</li>
                 ))}
             </ul>
-            <div className="flex gap-1">
-                <button onClick={() => append({ url: "" })}
-                    type="button"
-                    className="bg-blue-500 p-2 rounded text-blue-50 cursor-pointer w-full">
-                    Ajouter
-                </button>
+        </li>
+    ));
+
+    return (
+        <section className="bg-gray-200 border-4 border-dashed rounded-xl border-gray-600 p-4">
+            <div {...getRootProps({ className: 'dropzone' })}>
+                <input {...getInputProps()} />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+                <em>(Only *.jpeg and *.png images will be accepted)</em>
             </div>
-        </label>
-    </Card>;
+            <aside>
+                <h4>Accepted files</h4>
+                <ul>{acceptedFileItems}</ul>
+                <h4>Rejected files</h4>
+                <ul>{fileRejectionItems}</ul>
+            </aside>
+        </section>
+    );
+
+    // return <Card>
+    //     <label className="block">
+    //         <span className="text-gray-700">Images</span>
+    //         <ul>
+    //             {fields.map((field, index) => (
+    //                 <li key={field.id}>
+    //                     <input type="url" placeholder="https://..."
+    //                         {...register(`images.${index}.url` as const)}
+    //                     />
+    //                     <button type="button" onClick={() => remove(index)}>
+    //                         <X className="size-5" />
+    //                     </button>
+    //                 </li>
+    //             ))}
+    //         </ul>
+    //         <div className="flex gap-1">
+    //             <button onClick={() => append({ url: "" })}
+    //                 type="button"
+    //                 className="bg-blue-500 p-2 rounded text-blue-50 cursor-pointer w-full">
+    //                 Ajouter
+    //             </button>
+    //         </div>
+    //     </label>
+    // </Card>;
 }
